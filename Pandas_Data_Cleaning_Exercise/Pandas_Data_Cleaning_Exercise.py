89924d1463d72347c81533bd466d7f775b31f418
jdpaulk <jdp38@duke.edu>
2015-01-28 15:25:22 -0500

    Data Cleaning Exercise

---
+++ Pandas_Data_Cleaning_Exercise/Pandas_Data_Cleaning_Exercise.py
@@ -0,0 +1,10 @@
+from pandas import Series, DataFrame
+import pandas as pd
+import numpy as np
+
+main_dir = "C:\Users\Daniel\Desktop\Data"
+csv_file = "\sample_data_unclean.txt"
+git_dir = "C:\Users\Daniel\Documents\GitHub\PubPol590"
+
+
+
